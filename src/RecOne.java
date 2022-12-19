f78b9076fc132727f6474eb3dc7e6f02c2fe3f9c
SimoRBLX <91323541+SimoRBLX@users.noreply.github.com>
2022-12-19 16:40:56 +0100

    *

--- src/RecOne.java
+++ src/RecOne.java
@@ -1,5 +1,9 @@
+// Importo la LIBRERIA dello scanner da java.util
+import java.util.Scanner;
 public class RecOne
 {
+    // Dichiaro la variabile scanner
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
         // System.out.print() per  non usare spazio, inserire \n dentro una stringa per avviare lo spazio
