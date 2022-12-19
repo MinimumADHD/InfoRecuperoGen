6f0f8573d676d6144a7a13f854bd9e610ea208f8
SimoRBLX <91323541+SimoRBLX@users.noreply.github.com>
2022-12-19 17:05:42 +0100

    *

--- src/RecOne.java
+++ src/RecOne.java
@@ -1,4 +1,5 @@
 // Importo la LIBRERIA dello scanner da java.util
+import java.awt.*;
 import java.util.Scanner;
 public class RecOne
 {
@@ -10,5 +11,23 @@ public class RecOne
         System.out.print("UwU\n");
         // System.out.println() per usare auto spazio
         System.out.println("UwU");
+
+        // "richiamo" il metodo per usarlo
+        MetodoUno();
+    }
+    // Dichiaro metodo MetodoUno in static void (Si può usare per tutto)
+    static void MetodoUno()
+    {
+        // La variabile sc (scanner) prende input qualsiasi da tastiera, sc.nextLine() si usa di solito per le stringhe
+        // Dichiaro variabile int con valore da tastiera
+        int a,b;
+        // .nextInt() produce uno spazio che non ci deve essere nel numero
+        // a = sc.nextInt();
+        // Integer.parseInt non produce lo spazio
+        a = Integer.parseInt(sc.nextLine());
+        b = Integer.parseInt(sc.nextLine());
+
+        // Stampa a;b
+        System.out.println(a+";"+b);
     }
 }
