b8463e1727a8dd822f827763b9c5f3d6853b84d5
SimoRBLX <91323541+SimoRBLX@users.noreply.github.com>
2022-12-21 10:05:09 +0100

    *

---
+++ src/IntelliJShowup.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class IntelliJShowup
+{
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.println("CC");
+    }
+}
